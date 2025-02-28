f20640de4e762fe39a6d87716b73f45b8212ba12
dsriva03 <dsriva03@gmail.com>
2025-02-27 21:42:49 -0500

    adding in routes

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import Dashboard from './Dashboard';
-
+import {useNavigate} from 'react-router-dom'
 //create a useState that toggles the page?
 
 
 //create a handleClick func that changes state
 function LandingPage() {
-  
- const getRoute = async() => {
-  try {
 
-    const response = await fetch('/dashboard')
+const navigate = useNavigate();
 
-    if (!response.ok){
-      throw new Error("Error in getting dashboard route: " + response.status)
-    }
+const handleClick = () => {
+  navigate('/dashboard')
+}
 
-  } catch(err){
-    console.error("This is the error in getRoute: " + err)
-  }
- }
+//  const getRoute = async() => {
+//   try {
+
+//     const response = await fetch('/dashboard')
+
+//     if (!response.ok){
+//       throw new Error("Error in getting dashboard route: " + response.status)
+//     }
+
+//   } catch(err){
+//     console.error("This is the error in getRoute: " + err)
+//   }
+//  }
 
   return (
 
@@ -27,7 +33,7 @@ function LandingPage() {
     <main><h2>Whatcha workin' on?</h2></main>
     <input type="text" placeholder=""></input>
 
-    <button type="submit" onClick={getRoute}>Submit Your Question</button>
+    <button type="submit" onClick={handleClick}>Submit Your Question</button>
     </div>
     
   );
