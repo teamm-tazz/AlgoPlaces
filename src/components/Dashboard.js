ee579e9b9b49f695d8fb6217f768df51d39ae1a8
dsriva03 <dsriva03@gmail.com>
2025-03-01 12:51:51 -0500

    created route for dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,35 @@
 import React from 'react';
-
+import {useState} from 'react'
+import {useLocation} from 'react-router-dom'
+import {PracticeProblem} from './PracticeProblem'
+import {Approach} from './Approach'
+import {InputProblem} from './InputProblem'
 function Dashboard() {
+
+  const location = useLocation();
+  //extract userWQuery var sent to /dashboard from the landing page
+  const {userQuery} = location.state;
+  const [strategy, setStrategy] = useState('')
+  console.log('This is the query sent to us from da landing page:  ', userQuery)
+
+  //create a function to request the approach
+  const getStrategy = async() => {
+    const result = await apiFetch.requestStrategy(userQuery);
+    setStrategy(result)
+
+  }
+  useEffect(() => {
+    getStrategy()
+  })
+
   return (
-    <header>
-      <h1>My Web App</h1>
-    </header>
+    <div>
+ 
+      <h1>Dashboard</h1>
+      <h2>Your Query: {userQuery} </h2>
+
+
+    </div>
   );
 }
 
